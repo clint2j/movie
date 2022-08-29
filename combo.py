04ffb238c2737bb8589d40166e3267cce8e6fab3
clint2j <clinthart21@tamu.edu>
2022-08-28 23:52:34 -0500

    Halfway through combo

--- combo.py
+++ combo.py
@@ -1,6 +1,15 @@
 file = open("ratings.csv", "r")
 file2 = open("titles.csv", "r")
-for line in myfile:
-    l = line.split(",")
-    if (l[1]=="movie"):
-        file2.write(line)
+file3 = open("data.csv", "w")
+#Combine ratings and titles files into data with rating somewhere on each line and number of votes too
+l = 0
+for line in file2:
+    movie = line.split(",")
+    rating = 0
+    num = 0
+    ll = 0
+    for l2 in file[l:]:
+        rate = l2.split(",")
+        ll+=1
+        if rate[0]==movie[0]:
+            l = ll
